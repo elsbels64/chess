b236fe661085824d8579d3e6aafda2a330a55f08
Elise <ewwirthlin@gmail.com>
2024-11-14 14:06:29 -0700

    fixed before each test and added failure tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -23,8 +23,13 @@ public class ServerFacadeTests {
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         serverFacade = new ServerFacade("http://localhost:"+port);
+    }
+
+    @BeforeEach
+    public void SetUp() {
         serverFacade.clear();
         String auth = serverFacade.registerUser("Elise", "password", "email");
+        gamesList = new ArrayList<>();
         String GameIDStr = serverFacade.createGame("game1", auth);
         int gameID = Integer.parseInt(GameIDStr);
         gamesList.add(new GameData(gameID, null, null, "game1", new ChessGame()));
@@ -33,38 +38,6 @@ public class ServerFacadeTests {
     @AfterAll
     static void stopServer() {
         server.stop();
-    }
-
-    @Test
-    public void clearTest() {
-        String response = serverFacade.clear();
-
-    }
-
-    @Test
-    public void registerUserTest() {
-        String auth = serverFacade.registerUser("username", "password", "email");
-        System.out.println(auth);
-        Assertions.assertNotNull(auth);
-        Assertions.assertFalse(auth.startsWith("failure: "));
-    }
-
-    @Test
-    public void loginUserTest() {
-        String auth = serverFacade.loginUser("Elise", "password");
-        System.out.println(auth);
-        Assertions.assertNotNull(auth);
-        Assertions.assertFalse(auth.startsWith("failure: "));
-    }
-
-    @Test
-    public void logoutUserTest() {
-        String auth = serverFacade.loginUser("Elise", "password");
-        System.out.println(auth);
-        Assertions.assertNotNull(auth);
-        Assertions.assertFalse(auth.startsWith("failure: "));
-        String result = serverFacade.logoutUser(auth);
-        Assertions.assertEquals("You have been successfully logged out", result);
     }
 
     @Test
@@ -78,4 +51,71 @@ public class ServerFacadeTests {
         Assertions.assertEquals(gamesList, responseAndGamesList.gamesList());
     }
 
+    @Test
+    public void listGameFailureTest() {
+        String auth = serverFacade.loginUser("Elise", "password");
+        System.out.println(auth);
+        Assertions.assertNotNull(auth);
+        Assertions.assertFalse(auth.startsWith("failure: "));
+        ServerFacadeListGamesReturn responseAndGamesList = serverFacade.listGames("");
+        Assertions.assertTrue(responseAndGamesList.response().startsWith("failure: "));
+    }
+
+    @Test
+    public void registerUserTest() {
+        String auth = serverFacade.registerUser("username", "password", "email");
+        System.out.println(auth);
+        Assertions.assertNotNull(auth);
+        Assertions.assertFalse(auth.startsWith("failure: "));
+    }
+
+    @Test
+    public void registerUserFailureTest() {
+        String auth = serverFacade.registerUser("username", null, "email");
+        System.out.println(auth);
+        Assertions.assertNotNull(auth);
+        Assertions.assertTrue(auth.startsWith("failure: "));
+    }
+
+    @Test
+    public void loginUserTest() {
+        String auth = serverFacade.loginUser("Elise", "password");
+        System.out.println(auth);
+        Assertions.assertNotNull(auth);
+        Assertions.assertFalse(auth.startsWith("failure: "));
+    }
+
+    @Test
+    public void loginUserFailureTest() {
+        String auth = serverFacade.loginUser("Elise", "wrongPassword");
+        System.out.println(auth);
+        Assertions.assertNotNull(auth);
+        Assertions.assertTrue(auth.startsWith("failure: "));
+    }
+
+    @Test
+    public void logoutUserTest() {
+        String auth = serverFacade.loginUser("Elise", "password");
+        System.out.println(auth);
+        Assertions.assertNotNull(auth);
+        Assertions.assertFalse(auth.startsWith("failure: "));
+        String result = serverFacade.logoutUser(auth);
+        Assertions.assertEquals("You have been successfully logged out", result);
+    }
+
+    @Test
+    public void logoutUserFailureTest() {
+        String auth = serverFacade.loginUser("Elise", "password");
+        System.out.println(auth);
+        Assertions.assertNotNull(auth);
+        Assertions.assertFalse(auth.startsWith("failure: "));
+        String result = serverFacade.logoutUser("");
+        Assertions.assertTrue(result.startsWith("failure: "));
+    }
+
+    @Test
+    public void clearTest() {
+        String response = serverFacade.clear();
+        Assertions.assertFalse(response.startsWith("failure: "));
+    }
 }
