51ba97f914fd0d183da0b84a444c4c548dbceb6c
Elise <ewwirthlin@gmail.com>
2024-12-05 21:57:38 -0700

    everything SEEMS to be working

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -8,9 +8,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("♕ 240 Chess Client: ");
         Repl repl = new Repl("http://localhost:" + 8080);
-        ChessGame chessGame = new ChessGame();
-        var validMoves = chessGame.validMoves(new ChessPosition(2,1));
-        System.out.print(new DisplayBoard().printValidMovesWhite(chessGame.getBoard(),validMoves));
+//        ChessGame chessGame = new ChessGame();
+//        var validMoves = chessGame.validMoves(new ChessPosition(2,1));
+//        System.out.print(new DisplayBoard().printValidMovesWhite(chessGame.getBoard(),validMoves));
         System.out.println("Running Repl:");
         repl.run();
     }
